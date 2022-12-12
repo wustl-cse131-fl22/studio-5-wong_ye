21ba1f79cb6b0517f0f1539f28801950e943ed45
austinye <austinye@austins-air.dhcp.wustl.edu>
2022-12-12 02:50:49 -0600

    redo

---
+++ src/studio5/test1.java
@@ -0,0 +1,18 @@
+package studio5;
+
+import java.util.Scanner;
+
+public class test1 {
+	Scanner in = new Scanner(System.in);
+public static void main(String[] args) {
+System.out.println()
+	
+}
+
+	public static void inFo(String value, double height) {
+		double boy = height*0.3038;
+		double boy2 = (int) (boy*100)/100.0;
+		System.out.println(value + " in meters " + boy2);
+	}
+	
+}
